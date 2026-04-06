3056709e2e04c736c1a632a5d534c8f465317da7
MD RAIHAN <mdraihannahid@gmail.com>
2026-04-06 10:33:40 +0530

    updated server files

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,4 +1,4 @@
-import Message from "../models/message.js";
+import Message from "../models/Message.js";
 import User from "../models/User.js";
 import cloudinary from '../lib/cloudinary.js'
 import { io, userSocketMap } from "../server.js";
